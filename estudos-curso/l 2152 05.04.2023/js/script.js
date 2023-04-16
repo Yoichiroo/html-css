85ff4c0c46be37546c3d7a9722d00bd5fbe42ede
Lucas Torres <128598080+ProjetoMatin@users.noreply.github.com>
2023-04-16 13:19:19 -0300

    Estudos do curso

---
+++ estudos-curso/l 2152 05.04.2023/js/script.js
@@ -0,0 +1,29 @@
+function calc() {
+    let horarioFesta = document.formFesta.horario.value
+    if (horarioFesta == "manha") {
+        total = 1500.00
+    } else if (horarioFesta == "tarde") {
+        total = 1800.00
+    } else {
+        total = 2300.00
+    }
+    console.log(horarioFesta)
+    document.getElementById("totalOrcamento").value = total
+}
+
+function trocarTema() {
+    let temaFesta = document.getElementById("temaFesta").value
+    if (temaFesta == "flamengo") {
+        novoFundo = "img/flamengo.jpg"
+    } else if (temaFesta == "princesaSofia") {
+        novoFundo = "img/princesaSofia.jpg"
+    } else if (temaFesta == "CoD") {
+        novoFundo = "img/CoD.jpg"
+    } else if (temaFesta == "LoL") {
+        novoFundo = "img/LoL.jpg"
+    } else if (temaFesta == "FF") {
+        novoFundo = "img/freeFire.jpg"
+    }
+    document.body.style.backgroundImage = "url(" + novoFundo + ")"
+    console.log(temaFesta)
+}
